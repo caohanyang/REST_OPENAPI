dbd16a18d2681fa45cbf14cc4cc22ebdae9070ae
caohanyang <caohanyang007@gmail.com>
2017-11-21 15:04:02 +0100

    add validation into process

--- post_validation/refine.js
+++ post_validation/refine.js
@@ -1,6 +1,24 @@
 
-const openAPI = require('./openAPI7.json');
-var fs = require('fs');
+const fs = require('fs');
+var path = require('path');
+const args = process.argv;
+
+// get open api file
+var API_NAME, CompareSet_PATH;
+
+if (args.length > 0) {
+	API_NAME = args[2].split("//")[1].split("\.")[1];
+	console.log(API_NAME)
+	// google have several APIs
+	if (API_NAME.includes("google")) {
+		API_NAME = args[2].split("//")[1].split("/")[1];
+	}
+	CompareSet_PATH = "../CompareSet/" + API_NAME;
+}
+
+const openAPI = require(CompareSet_PATH + "/OpenAPI.json");
+var FinalSet_PATH = path.join(__dirname, "..", "/FinalSet/");
+var API_PATH = path.join(FinalSet_PATH, API_NAME);
 
 // 1. make sure it contains host && schema
 
@@ -16,6 +34,14 @@ if (openAPI.host && openAPI.schemes && openAPI.paths) {
 		}
 	}
 
-	fs.writeFileSync("refineOpenAPI.json", JSON.stringify(openAPI, null, 2), 'utf8');
+	console.log(FinalSet_PATH);
+	if (!fs.existsSync(FinalSet_PATH)) {
+		fs.mkdirSync(FinalSet_PATH);
+		if(!fs.existsSync(API_PATH)) {
+			fs.mkdirSync(API_PATH);
+		}
+	}
+
+	fs.writeFileSync(API_PATH + "/refineOpenAPI.json", JSON.stringify(openAPI, null, 2), 'utf8');
 }
 
